f7d8bf7db43bc035adc278696ceba73b75b8a4e1
Bob Lail <bob.lail@cph.org>
2014-11-05 11:53:42 -0600

    [refactor] Merged app.js and server.js (1m)

--- app/server.js
+++ app/server.js
@@ -1,4 +1,62 @@
-var app = require('../app/app');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes');
+require('./lib/handlebars_helpers');
+
+var app = express();
+
+var theme = process.env.THEME || 'blank';
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.set('view options', {layout: '../../themes/' + theme + '/default.theme'});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/assets', express.static(path.join(__dirname, 'assets'))); // assets supplied by Unite
+app.use('/theme', express.static(path.join(__dirname, '../themes/' + theme))); // assets supplied by the theme
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+
+// Server
 
 app.set('port', process.env.PORT || 3000);
 
